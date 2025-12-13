1d220213d996634584a6c10df0ffbc29af71ee8e
I Putu Gede Agastya Krisna Widartha <tiokrisna05@gmail.com>
2025-12-13 22:19:45 +0700

    kode

---
+++ include/child.h
@@ -0,0 +1,38 @@
+HEADER CHILD
+
+#ifndef CHILD_H_INCLUDED
+#define CHILD_H_INCLUDED
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Parent;
+struct Relasi;
+
+typedef struct Child *adrChild;
+
+struct Child {
+    string idSepatu;
+    string namaSepatu;
+    string jenis;
+    int harga;
+    int ukuran;
+
+    adrChild next;
+};
+
+struct ListChild {
+    adrChild first;
+};
+
+void createListChild(ListChild &L);
+adrChild alokasiChild(string id, string nama, string jenis, int harga, int ukuran);
+void insertChild(ListChild &L, adrChild P);
+void deleteChild(ListChild &L, string id);
+adrChild findChild(ListChild L, string id);
+void showAllChild(ListChild L);
+
+
+#endif // CHILD_H_INCLUDED
---
+++ include/parent.h
@@ -0,0 +1,34 @@
+HEADER PARENT
+
+#ifndef PARENT_H_INCLUDED
+#define PARENT_H_INCLUDED
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Child;
+struct Relasi;
+
+typedef struct Parent *adrParent;
+
+struct Parent {
+    int idMerk;
+    string namaMerk;
+    string negaraAsal;
+
+    adrParent next;
+};
+
+struct ListParent {
+    adrParent first;
+};
+
+void createListParent(ListParent &L);
+adrParent alokasiParent(int id, string nama, string negara);
+void insertParent(ListParent &L, adrParent P);
+void deleteParent(ListParent &L, int id);
+adrParent findParent(ListParent L, int id);
+void showAllParent(ListParent L);
+#endif // PARENT_H_INCLUDED
